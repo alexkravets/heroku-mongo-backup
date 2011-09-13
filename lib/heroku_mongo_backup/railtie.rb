9e7d37de1d4f387a8adbe2f4950d0b61d562d4bc
alexkravets <santyor@gmail.com>
2011-09-13 22:39:17 +0300

    module fix;

--- lib/heroku_mongo_backup/railtie.rb
+++ lib/heroku_mongo_backup/railtie.rb
@@ -1,14 +1,12 @@
 require 'heroku_mongo_backup'
 require 'rails'
 
-module Heroku
-  module Mongo
-    class Railtie < Rails::Railtie
-      railtie_name :heroku_mongo_backup
+module HerokuMongoBackup
+  class Railtie < Rails::Railtie
+    railtie_name :heroku_mongo_backup
 
-      rake_tasks do
-        load "tasks/heroku_mongo_backup.rake"
-      end
+    rake_tasks do
+      load "tasks/heroku_mongo_backup.rake"
     end
   end
 end
